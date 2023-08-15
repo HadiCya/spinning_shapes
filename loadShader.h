dc11128988c8739fed0b6f2c80ca62f57dc96781
HadiChaaban <chaabanh@umich.edu>
2023-08-15 19:08:07 -0400

    Part 2

---
+++ loadShader.h
@@ -0,0 +1,9 @@
+#ifndef LOAD_SHADER_H
+#define LOAD_SHADER_H
+
+#include <glad/glad.h>
+
+GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path);
+
+#endif // LOAD_SHADER_H
+
---
+++ mesh.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include "mesh.h"
+#include <SDL.h>
+#include <iostream>
+
+Mesh::Mesh() {
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	GLfloat verticies[] = {
+		0.5f, -0.5f, 0.5f,
+		-0.5f, -0.5f, 0.5f,
+		0.5f, 0.5f, 0.5f,
+		-0.5f, 0.5f, 0.5f
+	};
+
+	GLint triangles[] = {0, 2, 3, 0, 3, 1};
+
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &elementbuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
+
+	vertex_size = sizeof(triangles) / sizeof(GLint);
+}
+
+void Mesh::draw(){
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+	glDrawElements(GL_TRIANGLES, vertex_size, GL_UNSIGNED_INT, 0);
+
+	glDisableVertexAttribArray(0);
+}
